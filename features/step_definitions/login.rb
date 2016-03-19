8432ed7f0ade9a348519caddaffa4d0f9854a371
sharanya1505 <sharanyashivakumar150593@gmail.com>
2016-03-19 23:20:24 +0000

    cucumber login

---
+++ features/step_definitions/login.rb
@@ -0,0 +1,56 @@
+Given(/^the following login details exist in database:$/) do |table|
+  # table is a Cucumber::Ast::Table
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I am the Login page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I click on the "(.*?)" button$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see login page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should enter the "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should press "(.*?)" to login to the Bearcat Food Search account$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see the signup page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should enter "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should create a "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^i should create a "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^then click on the "(.*?)" to login$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I click on "(.*?)" button$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should get a message saying "(.*?)" on the entry of an invalid username or password$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see the "(.*?)" page again$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
