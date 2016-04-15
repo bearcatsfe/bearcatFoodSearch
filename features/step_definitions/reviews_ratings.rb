82181d2345b4ab91f565c46c36b6f8875406ca7f
Kushal Shinde <kshinde1@binghamton.edu>
2016-04-15 00:02:45 +0000

    Review and Ratings files added

---
+++ features/step_definitions/reviews_ratings.rb
@@ -0,0 +1,27 @@
+require 'uri'
+require 'cgi'
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "paths"))
+
+
+When(/^I select Food Outlet$/) do 
+  visit('/reviews_and_ratings/new')
+end
+
+When(/^I press "([^"]*)" button$/) do |button|
+  click_button(button)
+end
+
+
+Then(/^I should click on "([^"]*)" button to give user ratings$/) do |button|
+  #click_button(button)
+end
+
+Then(/^I should write "([^"]*)" text field to give user reviews$/) do |arg1|
+  #pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^the following food outlets exist:$/) do |table|
+end
+
+And(/^I selected particular Food Outlet$/) do
+end
