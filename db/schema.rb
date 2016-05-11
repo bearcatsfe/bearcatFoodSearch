73892f352e73356d834f717936858920956f1c31
ashah38 <ashah38@binghamton.edu>
2016-05-11 18:52:03 +0000

    Correct values in db

--- db/schema.rb
+++ db/schema.rb
@@ -21,8 +21,10 @@ ActiveRecord::Schema.define(version: 20160504132919) do
   end
 
   create_table "menu_lists", force: :cascade do |t|
-    t.string "menu_item"
-    t.string "price"
+    t.string   "menu_item"
+    t.string   "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "reviews_and_ratings", force: :cascade do |t|
