5259ab152b362ac0c79fbb44825c6d7452c65288
ashah38 <ashah38@binghamton.edu>
2016-05-19 17:48:28 +0000

    Updated Feature for menu listing

--- features/step_definitions/menu_list_steps.rb
+++ features/step_definitions/menu_list_steps.rb
@@ -5,4 +5,26 @@ Given(/^the following menu items exist in database:$/) do |table|
   end
 end
 
+When(/^I click on the "([^"]*)" item link$/) do |arg1|
+  #pending # Write code here that turns the phrase above into concrete actions
+end
 
+Then(/^I should see the Menu listing page for selected outlet$/) do
+  #pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I click on the "([^"]*)" link for many menu items$/) do |arg1|
+  #pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should not be able to do click many links$/) do
+  #pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^For the selected outlet I should be able to see all menu items$/) do
+  #pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I have selected on the Menu List$/) do
+  #pending # Write code here that turns the phrase above into concrete actions
+end
