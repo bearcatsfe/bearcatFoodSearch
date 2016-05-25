08791a6e7489b6611c2c4ef36cb904b825687480
sunilbinghamton <ssahu1@binghamton.edu>
2016-05-25 01:00:59 +0000

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160510011614) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "food_courts", force: :cascade do |t|
     t.string   "foodCourt"
     t.string   "distance"
@@ -52,6 +55,6 @@ ActiveRecord::Schema.define(version: 20160510011614) do
     t.string   "password_digest"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
